cc0def35a9d3200064d2b784f0bacc88315574b4
Vins0002 <149610754+Vins0002@users.noreply.github.com>
2023-11-24 18:46:28 +0100

    Update FactoryAction.java
    Ho aggiunto i commenti alla classe FactoryAction

--- src/FactoryAction.java
+++ src/FactoryAction.java
@@ -1,19 +1,26 @@
-public class FactoryAction{
+public class FactoryAction {
+    // Variabili di istanza private per memorizzare il nome dell'azione e il suo contenuto
     private String nameAction;
     private String content;
-
-    public FactoryAction(String nameAction, String content){
+ 
+    // Costruttore della classe che accetta il nome dell'azione e il contenuto come parametri
+    public FactoryAction(String nameAction, String content) {
+        // Inizializza le variabili di istanza con i valori forniti
         this.nameAction = nameAction;
         this.content = content;
     }
-
-    public Action FactoryAction(String actionType, String content) throws IllegalArgumentException{
-        if(actionType.equals("Promemoria")){
-            return new DisplayMessage(content);
-        }else if(actionType.equals("Sveglia")){
-            return new AlarmClock(content);
-        }else{
-            throw new IllegalArgumentException("Azione non valida: " + actionType);
+ 
+    // Metodo per creare un'istanza di una classe che implementa l'interfaccia Action
+    // in base al nome dell'azione fornito come parametro
+    public Action createConcreteAction(String nameAction, String content) throws IllegalArgumentException {
+        // Verifica il tipo di azione e restituisce un'istanza corrispondente
+        if (nameAction.equals("Promemoria")) {
+            return new DisplayMessage(content); // Crea e restituisce un oggetto DisplayMessage
+        } else if (nameAction.equals("Sveglia")) {
+            return new AlarmClock(content); // Crea e restituisce un oggetto AlarmClock
+        } else {
+            // Se il nome dell'azione non corrisponde a nessuno dei casi precedenti, lancia un'eccezione
+            throw new IllegalArgumentException("Azione non valida: " + nameAction);
         }
     }
 }
