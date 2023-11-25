02438a7564da66c574bae6023c0e8ea88a4a2b3f
Egidio0 <e.zotta2000@gmail.com>
2023-11-25 19:54:06 +0100

    Aggiunti i metodi get e set per trasmettere i dati nella pagina principale

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -5,45 +5,34 @@ import java.io.File;
 import java.io.IOException;
 
 public class AlarmClock extends FactoryAction implements Action{
-    private String audioFilePath;
     private Stage dialogStage;
     private Clip clip;
-
+    private String audioFilePath;
     public AlarmClock(String audioFilePath) {
         this.audioFilePath=audioFilePath;
-
-    }
-
-    @Override
-    public String getName() {
-        return "Sveglia";
-    }
-
-    @Override
-    public String getContent() {
-        return audioFilePath;
-    }
-
-    @Override
-    public void execute() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(audioFilePath));
-            //Creo un oggetto clip per riprodurre audioInputStream
+            //clip per riprodurre audioInputStream
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-            //Avvia la riproduzione continua dell'audio
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-
             dialogStage = new Stage();
             dialogStage.setTitle("Riproduzione Audio");
-            //Riproduzione dell'audio interrotta dalla chiusura
-            //della finestra di dialogo da parte dell'utente
-            dialogStage.setOnCloseRequest(event -> onActionClose());
-            dialogStage.showAndWait();
 
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
+
+    }
+
+    @Override
+    public void execute() {
+        dialogStage.show();
+        //Avvia la riproduzione continua dell'audio
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+        //Riproduzione dell'audio interrotta dalla chiusura
+        //della finestra di dialogo da parte dell'utente
+        dialogStage.setOnCloseRequest(event -> onActionClose());
     }
 
     @Override
@@ -56,5 +45,13 @@ public class AlarmClock extends FactoryAction implements Action{
             dialogStage.close();
         }
     }
+    @Override
+    public String getName() {
+        return "Sveglia";
+    }
 
+    @Override
+    public String getContent() {
+        return audioFilePath;
+    }
 }
