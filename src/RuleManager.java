0c48c603424600d9d377d4947a3e3e1c75648142
fabriziosatu <fabriziosatu2150@gmail.com>
2023-11-25 17:13:42 +0100

    Aggiunta del metodo check di RuleManager

--- src/RuleManager.java
+++ src/RuleManager.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class RuleManager {
     private final ObservableList<Rule> rules;
     private static RuleManager instance;
+    private Action action;
 
     private RuleManager(){
         rules = FXCollections.observableArrayList();
@@ -20,11 +21,34 @@ public class RuleManager {
         return instance;
     }
 
-    public void addRule(String nameRule, String nameAction, String nameTrigger, String content, LocalTime time){
-        this.rules.add(new Rule(nameRule, nameAction, nameTrigger, content, time));
+    public boolean check() {
+        LocalTime currentTime = LocalTime.now();
+        boolean triggerActivated = false;
+        for (Rule rule : rules) {
+            if (rule.getTrigger() instanceof TriggerTime) {
+                TriggerTime triggerTime = (TriggerTime) rule.getTrigger();
+                if (triggerTime.isTimeToTrigger(currentTime)) {
+                    if(rule.getAction() instanceof DisplayMessage) {
+                        DisplayMessage displayMessage = (DisplayMessage) rule.getAction();
+                        displayMessage.execute();
+                    } else if(rule.getAction() instanceof AlarmClock) {
+                             AlarmClock alarmClock = (AlarmClock) rule.getAction();
+                             alarmClock.execute();
+                    }
+                    triggerActivated = true;
+                }
+            }
+        }
+        return triggerActivated;
     }
 
-    public ObservableList<Rule> getRules(){
-        return this.rules;
+
+    public void addRule(String nameRule, String nameAction, String nameTrigger, String content, LocalTime time){
+        this.rules.add(new Rule(nameRule, nameAction, nameTrigger, content, time));
+        Rule rule = new Rule(nameRule, nameAction, nameTrigger, content, time);
     }
+
+
+
+    public ObservableList<Rule> getRules(){ return this.rules;}
 }
