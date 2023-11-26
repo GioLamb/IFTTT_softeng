8bd7f3fee019995fde8864e2d19918d8c3c49ce5
fabriziosatu <fabriziosatu2150@gmail.com>
2023-11-26 13:08:15 +0100

    Aggiunta la possibilità di nome nullo riguardo al trigger, e lanco di relativa eccezione.

--- src/FactoryTrigger.java
+++ src/FactoryTrigger.java
@@ -7,16 +7,33 @@ public class FactoryTrigger{
 
     //Costruttore della classe FactoryTrigger che accetta il nome del trigger e l'orario come parametri
     public FactoryTrigger(){};
+
     //Metodo per creare un'istanza di una classe che implementa l'interfaccia Trigger
     //in base al nome del trigger fornito come parametro
     public Trigger createConcreteTrigger(String nameTrigger, LocalTime time){
         //Verifica il tipo di trigger e restituisce un'istanza corrispondente
-        if(nameTrigger.equals("TriggerTime")){
-            return new TriggerTime(time);
-        }else{
-            //Se il nome del trigger non corrisponde a nessuno dei casi precedenti, lancia un'eccezione
-            throw new IllegalArgumentException("Trigger non valido: " + nameTrigger);
+        if (nameTrigger != null) {
+            // Verifica il tipo di trigger e restituisce un'istanza corrispondente
+            if (nameTrigger.equalsIgnoreCase("TriggerTime")) {
+                return new TriggerTime(time);
+            } else {
+                // Se il nome del trigger non corrisponde a nessuno dei casi precedenti, lancia un'eccezione
+                throw new IllegalArgumentException("Trigger non valido: " + nameTrigger);
+            }
+        } else {
+            // Se nameTrigger è nullo, lancia un'eccezione
+            throw new IllegalArgumentException("Il nome del trigger non può essere nullo");
         }
+
     }
 
+    /*
+    public String getContentTrigger() {
+        if (time != null) {
+            return time.toString(); // Restituisce l'orario del trigger come stringa
+        } else {
+            return null; // Nessun trigger è stato creato, quindi restituisce null
+        }
+    }
+    */
 }
