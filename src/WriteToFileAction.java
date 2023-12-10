17f3c95c6881a6f8e551445399deb367fa393c31
Vins0002 <vivi@192.168.1.8>
2023-12-10 13:30:08 +0100

    Update WriteToFileActionTest, WriteToFileAction

--- src/WriteToFileAction.java
+++ src/WriteToFileAction.java
@@ -8,29 +8,40 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFileAction extends FactoryAction implements Action {
+    // Percorso del file e contenuto da scrivere nel file
     private final String filePath;
     private final String content;
+
+    // Alert per la conferma e l'errore durante l'esecuzione
     private  Alert alertConfirm;
     Alert alertError;
 
+    // Costruttore della classe
     public WriteToFileAction(String filePath, String content) {
         this.filePath = filePath;
         this.content = content;
-        Platform.runLater(()->{
+
+        // Inizializzazione degli alert all'interno del thread JavaFX
+        Platform.runLater(() -> {
+            // Alert per la conferma
             this.alertConfirm = new Alert(Alert.AlertType.INFORMATION);
             this.alertConfirm.setTitle("AZIONE ESEGUITA");
             this.alertConfirm.setHeaderText(null);
-            this.alertConfirm.setContentText("Il contenuto è stato scritto nel file " +filePath);
+            this.alertConfirm.setContentText("Il contenuto è stato scritto nel file " + filePath);
+
+            // Alert per l'errore
             this.alertError = new Alert(Alert.AlertType.ERROR);
             alertError.setContentText("Il file non esiste o non è un file regolare (directory)");
         });
     }
 
+    // Metodo per ottenere il nome dell'azione
     @Override
     public String getName() {
         return "WriteToFileAction";
     }
 
+    // Metodi per ottenere il contenuto del percorso del file e il contenuto da scrivere
     @Override
     public String getContent1() {
         return filePath;
@@ -41,42 +52,49 @@ public class WriteToFileAction extends FactoryAction implements Action {
         return content;
     }
 
-
-
+    // Metodo per eseguire l'azione di scrittura nel file
     @Override
     public void execute() {
-        Platform.runLater(()->{
+        Platform.runLater(() -> {
+            // Creazione di un oggetto File con il percorso specificato
             File file = new File(filePath);
-            if(file.exists() && file.isFile()) {
+
+            // Verifica se il file esiste ed è un file regolare
+            if (file.exists() && file.isFile()) {
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                    // Scrittura del contenuto nel file
                     writer.write(content);
+
+                    // Mostra l'alert di conferma e gestisci la chiusura dell'alert
                     alertConfirm.show();
                     alertConfirm.setOnCloseRequest(event -> onActionClose());
                 } catch (IOException e) {
+                    // Gestione dell'errore di scrittura nel file
                     System.out.println("Si è verificato un errore durante la scrittura del contenuto nel file.");
                     e.printStackTrace();
                 }
-            }else {
+            } else {
+                // Mostra l'alert di errore e gestisci la chiusura dell'alert
                 alertError.show();
                 alertError.setOnCloseRequest(event -> onActionClose());
             }
         });
     }
 
+    // Metodo chiamato alla chiusura dell'azione
     @Override
     public void onActionClose() {
         Platform.runLater(() -> {
-            //chiusura alert con il clic del bottone OK dell'alert
-            //o con il click del bottone di chiusura dell'alert
+            // Chiusura dell'alert di conferma con il clic del bottone OK o di chiusura
             alertConfirm.setOnCloseRequest(event -> {
                 if (alertConfirm.getResult() == ButtonType.CLOSE || alertConfirm.getResult() == ButtonType.OK) {
                     alertConfirm.close();
                 }
             });
         });
+
         Platform.runLater(() -> {
-            //chiusura alert con il clic del bottone OK dell'alert
-            //o con il click del bottone di chiusura dell'alert
+            // Chiusura dell'alert di errore con il clic del bottone OK o di chiusura
             alertError.setOnCloseRequest(event -> {
                 if (alertError.getResult() == ButtonType.CLOSE || alertError.getResult() == ButtonType.OK) {
                     alertError.close();
@@ -85,4 +103,3 @@ public class WriteToFileAction extends FactoryAction implements Action {
         });
     }
 }
-
--- test/WriteToFileActionTest.java
+++ test/WriteToFileActionTest.java
@@ -12,30 +12,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WriteToFileActionTest {
 
+    // Percorso del file di test e contenuto di test
     private static final String TEST_FILE_PATH = "/Users/vivi/Documents/Text.txt";
     private static final String TEST_CONTENT = "Ciao";
     private WriteToFileAction writeToFileAction;
 
+    // Inizializza JavaFX prima di eseguire i test
     @BeforeAll
-    public static void initJFX(){
+    public static void initJFX() {
         System.setProperty("javafx.headless", "true");
         new JFXPanel();
     }
+
+    // Esegui prima di ogni singolo test
     @BeforeEach
     void setUp() {
+        // Crea un'istanza di WriteToFileAction con il percorso e il contenuto di test
         writeToFileAction = new WriteToFileAction(TEST_FILE_PATH, TEST_CONTENT);
     }
 
+    // Testa l'esecuzione dell'azione di scrittura nel file
     @Test
     void testExecute() {
+        // Esegui l'azione
         writeToFileAction.execute();
 
-
+        // Verifica che il file sia stato creato con successo
         File file = new File(TEST_FILE_PATH);
-
         assertTrue(file.exists());
 
-
+        // Leggi il contenuto del file e verifica che corrisponda al contenuto di test
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String fileContent = reader.readLine();
             assertEquals(TEST_CONTENT, fileContent);
@@ -44,20 +50,26 @@ public class WriteToFileActionTest {
         }
     }
 
+    // Testa il metodo getContent1() che restituisce il percorso del file
     @Test
     void testGetContent1() {
+        // Crea un'istanza di WriteToFileAction e verifica che il percorso sia corretto
         WriteToFileAction writeToFileAction = new WriteToFileAction(TEST_FILE_PATH, TEST_CONTENT);
         assertEquals(TEST_FILE_PATH, writeToFileAction.getContent1());
     }
 
+    // Testa il metodo getContent2() che restituisce il contenuto da scrivere nel file
     @Test
     void testGetContent2() {
+        // Crea un'istanza di WriteToFileAction e verifica che il contenuto sia corretto
         WriteToFileAction writeToFileAction = new WriteToFileAction(TEST_FILE_PATH, TEST_CONTENT);
         assertEquals(TEST_CONTENT, writeToFileAction.getContent2());
     }
 
+    // Testa il metodo onActionClose() senza generare eccezioni
     @Test
     void testOnActionClose() {
+        // Verifica che il metodo non generi eccezioni
         assertDoesNotThrow(() -> writeToFileAction.onActionClose());
     }
 }
